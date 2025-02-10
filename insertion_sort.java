6bea9fb9a422f6680c69b3e38ab37536862662a6
Diego-glitch-cloud <dcalderon918@gmail.com>
2025-02-09 23:53:35 -0600

    insertion sort comentado

--- insertion_sort.java
+++ insertion_sort.java
@@ -3,18 +3,24 @@ public class insertion_sort {
 
     public void sort(int[] array) {
         
+        // Tamaño del array
         int n = array.length;
 
         for (int i = 1; i < n; i++) {
+            // Toma el elemento actual como la key
             int key = array[i];
+            // Índice del elemento anterior a key
             int j = i - 1;
 
+            // Mueve los elementos del array que son mayores que key un lugar a la derecha para hacer espacio para key
             while (j >= 0 && array[j] > key) {
+                // Desplaza el elemento una posición a la derecha
                 array[j + 1] = array[j];
                 j = j - 1;
             }
+
+            // Pone la key en su posición correcta
             array[j + 1] = key;
         }
     }
-
 }
