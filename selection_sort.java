b53bd3288e1d08eeae771ee1e560c97227a7ff50
Diego-glitch-cloud <dcalderon918@gmail.com>
2025-02-10 00:01:49 -0600

    selection sort como sort opcional

--- selection_sort.java
+++ selection_sort.java
@@ -1,5 +1,29 @@
 
 
 public class selection_sort {
-    
+
+    public void sort(int[] array) {
+        int n = array.length;
+
+        // Recorre todo el array
+        for (int i = 0; i < n - 1; i++) {
+
+            // al final este será el index del elemento menor de todo el array
+            int minIndex = i;
+
+            // Segundo for que recorre el array un espacio más arriba
+            for (int j = i + 1; j < n; j++) {
+                // Si el elemento siguiente es menor al elemento actual
+                if (array[j] < array[minIndex]) {
+                    // el indice del elemento menor 
+                    minIndex = j;
+                }
+            }
+
+            // Intercambia el elemento mínimo encontrado con el primer elemento de la iteración principal
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+    }
 }
